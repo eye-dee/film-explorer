586afe44a33a338d4d52bdf7cc2c438e2ea3fdd0
Igor Drozdov <igor.drozdov@iii.com>
2018-09-24 00:28:25 +0300

    Discovery and Config completed

--- config/src/main/java/igor/film/ConfigApplication.java
+++ config/src/main/java/igor/film/ConfigApplication.java
@@ -2,13 +2,15 @@ package igor.film;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-@EnableDiscoveryClient
-public class SaveApplication {
+public class ConfigApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SaveApplication.class, args);
+    SpringApplication.run(ConfigApplication.class, args);
   }
+
 }
+
